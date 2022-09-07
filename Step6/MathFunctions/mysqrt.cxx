34227319a984d59dad0ec0ac27982661bf548203
liuji <liuji@liuji.localdomain>
2022-09-07 19:44:21 +0800

    step6

--- Step6/MathFunctions/mysqrt.cxx
+++ Step6/MathFunctions/mysqrt.cxx
@@ -1,32 +1,56 @@
-#include <cmath>
+// #include <cmath>
 #include <iostream>
 
 #include "MathFunctions.h"
+#include "Table.h"
 
 // a hack square root calculation using simple operations
-double mysqrt(double x)
-{
-  if (x <= 0) {
-    return 0;
-  }
+// double mysqrt(double x) {
+//     if (x <= 0) {
+//         return 0;
+//     }
 
-  // if we have both log and exp then use them
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-  double result = std::exp(std::log(x) * 0.5);
-  std::cout << "Computing sqrt of " << x << " to be " << result
-            << " using log and exp" << std::endl;
-#else
-  double result = x;
+//     // if we have both log and exp then use them
+//     // #if defined(HAVE_LOG) && defined(HAVE_EXP)
+//     //   double result = std::exp(std::log(x) * 0.5);
+//     //   std::cout << "Computing sqrt of " << x << " to be " << result
+//     //             << " using log and exp" << std::endl;
+//     // #else
+//     double result = x;
 
-  // do ten iterations
-  for (int i = 0; i < 10; ++i) {
-    if (result <= 0) {
-      result = 0.1;
+//     // do ten iterations
+//     for (int i = 0; i < 10; ++i) {
+//         if (result <= 0) {
+//             result = 0.1;
+//         }
+//         double delta = x - (result * result);
+//         result = result + 0.5 * delta / result;
+//         std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
+//     }
+
+//     return result;
+// }
+double mysqrt(double x) {
+    if (x <= 0) {
+        return 0;
     }
-    double delta = x - (result * result);
-    result = result + 0.5 * delta / result;
-    std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
-  }
-#endif
-  return result;
+
+    // use the table to help find an initial value
+    double result = x;
+    if (x >= 1 && x < 10) {
+        std::cout << "Use the table to help find an initial value " << std::endl;
+        result = sqrtTable[static_cast<int>(x)];
+    } else {
+        // do ten iterations
+        for (int i = 0; i < 10; ++i) {
+            if (result <= 0) {
+                result = 0.1;
+            }
+            double delta = x - (result * result);
+            result = result + 0.5 * delta / result;
+            std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
+        }
+    }
+
+    return result;
 }
