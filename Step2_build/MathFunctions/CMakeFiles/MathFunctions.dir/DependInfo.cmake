d10423f4329cb1a439138ff1c665437218679bcd
liuji <1546243245@qq.com>
2022-09-07 14:27:01 +0800

    step_2

---
+++ Step2_build/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liuji/cmake-3.24.1-tutorial-source/cmake-3.24.1-tutorial-source/Step2/MathFunctions/mysqrt.cxx" "/home/liuji/cmake-3.24.1-tutorial-source/cmake-3.24.1-tutorial-source/Step2_build/MathFunctions/CMakeFiles/MathFunctions.dir/mysqrt.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
