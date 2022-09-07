03087d374425cca176581b1bf5798fe54123a141
liuji <liuji@liuji.localdomain>
2022-09-07 21:27:01 +0800

    step12

---
+++ Step12_build/MathFunctions/CMakeFiles/MathFunctions.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../libMathFunctions.pdb"
+  "../libMathFunctions.so"
+  "../libMathFunctions.so.1"
+  "../libMathFunctions.so.1.0.0"
+  "CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o"
+  "CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathFunctions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
